96fcade7b28aebd8347aa72e1f8368fdd19b5cb2
misir8 <misiraskerov1@gmail.com>
2022-04-26 14:55:05 +0400

    feat:add rate.ts table

---
+++ src/entity/Rate.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'rates',
+})
+export class Rate {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('decimal', { precision: 5, scale: 2 })
+  buyRate: number;
+
+  @Column('decimal', { precision: 5, scale: 2 })
+  sellRate: number;
+
+  @Column({ default: false })
+  isActive: Boolean;
+}
